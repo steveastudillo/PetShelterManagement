436306d176b5aaf09171ec9c0bd8e96e261a5472
steve <steveastudillo121501@gmail.com>
2026-06-29 21:58:54 -0700

    Implemented adoption

--- app/src/main/java/org/example/Shelter.java
+++ app/src/main/java/org/example/Shelter.java
@@ -6,21 +6,28 @@ import java.util.List;
 public class Shelter {
 
     private final List<Animal> animals;
+    private final List<Adopter> adopters;
     private final List<Staff> staff;
 
     public Shelter() {
         animals = new ArrayList<>();
         staff = new ArrayList<>();
+        adopters = new ArrayList<>();
+    }
+    public void initializeAdopters() {
+        adopters.add(new Adopter("John", AnimalType.DOG));
+        adopters.add(new Adopter("Alice", AnimalType.CAT));
     }
     public void initializeStaff() {
         staff.add(new Veterinarian("Dr. Smith", 5));
         staff.add(new AdoptionCounselor("Emma", 3));
-}
+    }
 
 public void runSimulation() {
     System.out.println("-=-=-=- Pet Shelter Simulation -=-=-=-");
     initializeAnimals();
     initializeStaff();
+    initializeAdopters();
     for (int day = 1; day <= 7; day++) {
         System.out.println("\n-=-=-=- DAY " + day + " -=-=-=-");
         processDay(day);
@@ -34,6 +41,7 @@ public void processDay(int day) {
         for (Animal animal : animals) {
             animal.setCurrentState(new AvailableState());
         }
+
     } else if (day == 2) {
         System.out.println("A new stray dog arrived.");
         System.out.println(staff.get(0).getName() + " completed Rocky's intake examination.");
@@ -43,12 +51,16 @@ public void processDay(int day) {
                 "Rocky",
                 2,
                 "Healthy"));
+
     } else if (day == 3) {
-        System.out.println("Buddy has an adoption application.");
-        System.out.println(staff.get(1).getName() + " is reviewing Buddy's adoption application.");
+        Adopter adopter = adopters.get(0);
+        System.out.println(adopter.getName() + " wants to adopt a " + adopter.getPreferredAnimal() + ".");
+        System.out.println("Buddy matched with " + adopter.getName() + ".");
+        System.out.println(staff.get(1).getName() + " is reviewing the application.");
         animals.get(0).setCurrentState(new PendingState());
+
     } else if (day == 4) {
-        System.out.println("Buddy was adopted!");
+        System.out.println("Buddy was adopted by " + adopters.get(0).getName() + ".");
         System.out.println(staff.get(1).getName() + " finalized Buddy's adoption.");
         animals.get(0).setCurrentState(new AdoptedState());
     } else {
